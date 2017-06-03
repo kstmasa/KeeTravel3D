8517c5074f1b0f6dbcd7b0a2160052f2bbcd559c
GTI <af24520985@gmail.com>
2017-06-03 12:22:38 +0800

    version 1.3
    #美化首頁

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,12 +2,14 @@
  * Created by GTI on 2017/5/20.
  */
 var panorama;
-function initialize() {
-    panorama = new google.maps.StreetViewPanorama(
+var current = 0;
+function initialize(){
+        panorama = new google.maps.StreetViewPanorama(
         document.getElementById('street-view'),
         {
+
             position: {lat:  25.164858, lng: 121.727217},
-            pov: {heading: 165, pitch: 0},
+            pov: {heading: 1, pitch: 0},
             zoom: 1,
             zoomControl:false,
             panControl:false,
@@ -16,4 +18,23 @@ function initialize() {
             linksControl:false,
             fullscreenControl:false
         });
+        moveForward();
+}
+function difference(link) {
+    return Math.abs(panorama.pov.heading%360 - link.heading);
+}
+
+function moveForward() {
+    var curr;
+    for(i=0; i < panorama.links.length; i++) {
+        var differ = difference(panorama.links[i]);
+        if(curr == undefined) {
+            curr = panorama.links[i];
+        }
+
+        if(difference(curr) > difference(panorama.links[i])) {
+            curr = curr = panorama.links[i];
+        }
+    }
+    panorama.setPano(curr.panorama);
 }
