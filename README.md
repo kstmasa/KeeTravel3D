9ed9864905eaead93894e32a9b19a0c28bbac90a
GTI <af24520985@gmail.com>
2017-06-04 03:00:48 +0800

    version 1.4
    #改善 動畫

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KeeTravel3D
-travel game =>
+
+index=> {initial_page=>{status=>'play', status=>'exits'}, 
+		 keelung_city_page=>{left_top_div=>'current_time', middle_top_div=>'score', right_top_div=>'shop'
+							 middle_content_div=>'keelung_city', right_down_div=>'door_to_exit'}
+		 choose_景點_level_page=>{}}
--- asset/js/main.js
+++ asset/js/main.js
@@ -3,38 +3,46 @@
  */
 var panorama;
 var current = 0;
-function initialize(){
-        panorama = new google.maps.StreetViewPanorama(
-        document.getElementById('street-view'),
-        {
 
-            position: {lat:  25.164858, lng: 121.727217},
-            pov: {heading: 1, pitch: 0},
-            zoom: 1,
-            zoomControl:false,
-            panControl:false,
-            addressControl:false,
-            motionTrackingControl:false,
-            linksControl:false,
-            fullscreenControl:false
-        });
-        moveForward();
-}
-function difference(link) {
-    return Math.abs(panorama.pov.heading%360 - link.heading);
-}
+window.addEventListener("load",function(){
+	initialize();
+	$("#play_button").on("click",function(){
+		$(".initial_page").hide();
+		$(".keelung_city_page").show();
+	});
+	function initialize(){
+			panorama = new google.maps.StreetViewPanorama(
+			document.getElementById('street_view'),
+			{
+				position: {lat:  25.164858, lng: 121.727217},
+				pov: {heading: 1, pitch: 0},
+				zoom: 1,
+				zoomControl:false,
+				panControl:false,
+				addressControl:false,
+				motionTrackingControl:false,
+				linksControl:false,
+				fullscreenControl:false
+			});
+			moveForward();
+	}
+	function difference(link) {
+		return Math.abs(panorama.pov.heading%360 - link.heading);
+	}
 
-function moveForward() {
-    var curr;
-    for(i=0; i < panorama.links.length; i++) {
-        var differ = difference(panorama.links[i]);
-        if(curr == undefined) {
-            curr = panorama.links[i];
-        }
+	function moveForward() {
+		var curr;
+		for(i=0; i < panorama.links.length; i++) {
+			var differ = difference(panorama.links[i]);
+			if(curr == undefined) {
+				curr = panorama.links[i];
+			}
 
-        if(difference(curr) > difference(panorama.links[i])) {
-            curr = curr = panorama.links[i];
-        }
-    }
-    panorama.setPano(curr.panorama);
-}
+			if(difference(curr) > difference(panorama.links[i])) {
+				curr = panorama.links[i];
+			}
+		}
+		panorama.setPano(curr.panorama);
+	}
+
+});
